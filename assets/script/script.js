2001ac2f6166f1990b2e1fffef4e6e63c0fcf14a
Alexandre <alex67470@gmail.com>
2026-02-03 16:20:49 +0100

    feat(add) get modal feature ok

--- assets/script/script.js
+++ assets/script/script.js
@@ -25,8 +25,9 @@ function renderRecipes(recipes) {
         <ul>
           ${ingredientsHtml}
         </ul>
-        <button class="recipe-button"
-        <a href="recette.html?id=${recipe.id}">Voir la recette</a> </button>
+        <button class="openModalBtn recipe-button" data-id="${recipe.id}" type="button">
+            Voir la recette
+        </button>
       </article>
     `;
   });
@@ -34,4 +35,49 @@ function renderRecipes(recipes) {
 
 getRecipes().then((recipes) => {
   renderRecipes(recipes);
+});
+
+function openModal(recipe) {
+  const dialog = document.querySelector("#recipeModal");
+
+  document.querySelector("#modalName").textContent = recipe.name || "";
+  document.querySelector("#modalTime").textContent = "Temps : " + (recipe.time || "");
+  document.querySelector("#modalDescription").textContent = recipe.description || "";
+  document.querySelector("#modalAppliance").textContent = "Appareil : " + (recipe.appliance || "");
+ 
+  const ustensilsUl = document.querySelector("#modalUstensils");
+  ustensilsUl.innerHTML = ""; //! Clear previous content for each loop
+  (recipe.ustensils || []).forEach((u) => {
+    ustensilsUl.innerHTML += `<li>${u}</li>`;
+  });
+
+  const ingUl = document.querySelector("#modalIngredients");
+  ingUl.innerHTML = "";
+  recipe.ingredients.forEach((ing) => {
+    const qty = ing.quantity || "";
+    const unit = ing.unit || "";
+    const name = ing.ingredient || "";
+    ingUl.innerHTML += `<li>${qty} ${unit} ${name}</li>`;
+  });
+
+  dialog.showModal(); 
+}
+
+getRecipes().then((recipes) => {
+  renderRecipes(recipes);
+
+document.querySelector("#recipesContainer").addEventListener("click", (e) => {
+  if (e.target.classList.contains("openModalBtn")) {
+    const id = Number(e.target.dataset.id);
+    const recipe = recipes.find((r) => r.id === id); 
+    if (recipe) {
+      openModal(recipe);
+    }
+  }
+});
+
+  const dialog = document.querySelector("#recipeModal");
+  document.querySelector("#closeModal").addEventListener("click", () => {
+    dialog.close();
+  });
 });
